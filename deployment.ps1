4f90fa2d11c4995d02589094fa6229e60349f3e9
Todd Nelson <48332052+toddnelson5@users.noreply.github.com>
2022-08-19 11:39:59 -0400

    PS deployment

--- deployment.ps1
+++ deployment.ps1
@@ -1,3 +1,5 @@
 az login
 
-az deployment sub create --location "East US" --template-file ./foundationsdeploy.bicep  
+az deployment sub create --location "East US" --template-file ./foundationsdeploy.bicep 
+
+az deployment sub create --location "East US" --template-file ./main.bicep --template-parameter-file ./parameters.foundationsdeploy.bicep 
