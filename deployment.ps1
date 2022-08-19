cb9acb4128a8079e3518213f3d110e4de64abe8b
Todd Nelson <48332052+toddnelson5@users.noreply.github.com>
2022-08-19 12:03:12 -0400

    parameter changes and deployment file

--- deployment.ps1
+++ deployment.ps1
@@ -2,4 +2,4 @@ az login
 
 az deployment sub create --location "East US" --template-file ./foundationsdeploy.bicep 
 
-az deployment sub create --location "East US" --template-file ./main.bicep --template-parameter-file ./parameters.foundationsdeploy.bicep 
+az deployment sub create --location "East US" --template-file ./main.bicep --parameters ./main.parameters.json
