0633ea89621c4fdb0af158724873c08bd19c6bdb
Todd Nelson <48332052+toddnelson5@users.noreply.github.com>
2023-02-14 13:48:22 -0500

    Delete deployment.ps1

--- deployment.ps1
+++
@@ -1,5 +0,0 @@
-az login
-
-az deployment sub create --location "East US" --template-file ./foundationsdeploy.bicep 
-
-az deployment sub create --location "East US" --template-file ./main.bicep --parameters ./main.parameters.json
